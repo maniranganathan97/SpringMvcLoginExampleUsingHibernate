697293787c7b519f159412bdbaa7c97635437867
Mani Ranganathan <MRanganathan@flatironssolutions.com>
2019-06-06 15:16:41 +0530

    Test commit only

--- src/main/java/com/springDemo/SpringMVCDemo.java
+++ src/main/java/com/springDemo/SpringMVCDemo.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 // Git pull request Test
+// Git pull request Test2
 @Controller
 @RequestMapping(value="/")
 public class SpringMVCDemo {
